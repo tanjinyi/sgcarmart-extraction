4c578619604a97062308464f00d9180266256eb3
Huirong <huirong.tan.2017@sis.smu.edu.sg>
2018-12-12 16:13:48 +0800

    Added a helper regex to remove characters

---
+++ src/main/java/ai/preferred/crawler/sgcarmart/Helper.java
@@ -0,0 +1,10 @@
+package ai.preferred.crawler.sgcarmart;
+public class Helper {
+
+    //remove symbols and letters in numbers.
+    public int parseStringtoNum(String aString){
+        String newString = aString.replaceAll("[^0-9]","");
+        int result = Integer.parseInt(newString);
+        return result;
+    }
+}
